0bf0f98c9eab985e608cadaecbd61f05a2889f54
bob <ehdrud1332@gmail.com>
2020-04-04 11:27:19 +0900

    add state 상태값 선언

--- src/App.js
+++ src/App.js
@@ -9,11 +9,22 @@ import React, {Component} from 'react';
 
 
 
-
+//컴포넌트를 확장해주는 클래스 app
 class App extends Component {
 
     // 함수, 상수, 상태값을 선언해주는 위치
-
+    // 상태값은 상태값을 담는 그릇
+    state = {
+        count: 0
+    }
+    add = () => {
+        console.log("add");
+        this.setState(current => ({ count: current.count + 1 }));
+    }
+    Minus = () => {
+        console.log("Minus");
+        this.setState(current => ({ count: current.count - 1 }));
+    }
 
     render() {
 
@@ -22,8 +33,11 @@ class App extends Component {
         return (
             //화면에 뿌려주는 코드
             <div>
-
-            </div>
+                <h1>The number is {this.state.count}</h1>
+                {/*//button안에 onclick이라는 속성이 있음*/}
+                <button onClick={this.add}>add</button>
+                <button onClick={this.Minus}>Minus</button>
+           </div>
         );
     }
 }

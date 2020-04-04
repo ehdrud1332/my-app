4ca6df4d9d4873ed7e114e251026369a02fe58d1
bob <ehdrud1332@gmail.com>
2020-04-04 10:50:23 +0900

    Protection with PropType

--- src/potato.js
+++ src/potato.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Potato({ name, picture }) {
+function Potato({ name, picture, rating }) {
     return (
         <div>
             <h1> i love {name} </h1>
+            <h4>{rating} / 5.0</h4>
             <img src={picture}/>
         </div>
     );
 }
+// potato propTypes를 선언
+Potato.propTypes = {
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    rating: PropTypes.number
+};
+
 
 export default Potato;

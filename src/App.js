60a7ab6f513e2f2f2c2ee2d1e4fcb5ad1c123e18
bob <ehdrud1332@gmail.com>
2020-04-04 11:01:20 +0900

    extend Component Class 기본 구조

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
-import React from 'react';
-import Potato from "./potato";
 
-const foodILike = [
-    {
-        id: 1,
-        name: "Kimchi",
-        rating: 4.0,
-        image:
-            "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-    },
-    {
-        id:2,
-        name: "Samgyeopsal",
-        rating: 4.4,
-        image:
-            "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-    },
-    {
-        id:3,
-        name: "Bibimbap",
-        rating: 5.0,
-        image:
-            "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-    },
-    {
-        id:4,
-        name: "Doncasu",
-        rating: 5.0,
-        image:
-            "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-    },
-    {
-        id: 5,
-        name: "Kimbap",
-        rating: 5.0,
-        image:
-            "http://cdn2.koreanbasang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+//콤포넌트의 종류 함수,상수,클래스형
+//함수랑 상수랑 거의 비슷
+//클래스형
+
+// 단축키 rcc
+import React, {Component} from 'react';
+
+
+
+
+
+class App extends Component {
+
+    // 함수, 상수, 상태값을 선언해주는 위치
+
+
+    render() {
+
+        // return에서 사용될 상수 상태값들을 재선언해주는 곳
+
+        return (
+            //화면에 뿌려주는 코드
+            <div>
+
+            </div>
+        );
     }
-];
-
-function App() {
-  return (
-    <div>
-      <text>
-        jaemin app
-      </text>
-        {foodILike.map(dish => (
-            <Potato key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-        ))}
-
-    </div>
-  );
 }
 
 export default App;

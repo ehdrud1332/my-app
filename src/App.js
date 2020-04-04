7c2b34f1e6505b4ad060bad3f0625fecd006ccc9
bob <ehdrud1332@gmail.com>
2020-04-04 10:11:02 +0900

    my first component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Potato from "./potato";
 
 function App() {
   return (
@@ -6,6 +7,9 @@ function App() {
       <text>
         jaemin app
       </text>
+
+        <Potato/>
+        <Potato/>
 
     </div>
   );
